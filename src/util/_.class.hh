785767f64385d96bbced4ab9518ab7b85e2b9b42
Henning Janßen <development@henning-janssen.net>
2016-06-12 23:15:19 +0200

    Add BrokenInstallationException und _::SETTINGS and config-file
    A BIE indicates that some configuration has to be done
    f.e. important config-value is missing, config-file is malformed, etc.
    
    _::SETTINGS is used to access settings read from the _project-root_/config/config.json-file

--- src/util/_.class.hh
+++ src/util/_.class.hh
@@ -2,7 +2,10 @@
 
 namespace axolotl\util;
 
+use axolotl\exceptions\BrokenInstallationException;
+
 class _{
+  private static array<string, mixed> $settings = array();
   public static function GET(string $key): mixed{
     return $_GET[$key] ?? null;
   }
@@ -11,10 +14,30 @@ class _{
     return $_POST[$key] ?? null;
   }
 
+  public static function SERVER(string $key): mixed{
+    return $_SERVER[$key] ?? null;
+  }
+
   public static function SESSION(string $key, mixed $value = null): mixed{
     if($value === null){
       return $_SESSION["axolotl"][$key] ?? null;
     }
     return $_SESSION["axolotl"][$key] = $value;
+  }
+
+  public static function SETTINGS(string $key){
+    if(count(self::$settings) === 0){
+      $configPath = realpath(__DIR__.'/../../config/config.json');
+      if(!file_exists($configPath)){
+        throw new BrokenInstallationException("config-file is missing");
+      }
+      self::$settings = json_decode(
+        file_get_contents($configPath)
+      );
+      if(!is_array(self::$settings) || count(self::$settings) === 0){
+        throw new BrokenInstallationException("config-file is malformed");
+      }
+    }
+    return self::$settings[$key] ?? null;
   }
 }
