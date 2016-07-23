38b2e37053e764f0fbd8552d24f79a90000d29e2
Henning Janßen <development@henning-janssen.net>
2016-07-23 12:47:40 +0200

    Fix Homescreen
    - use full classname for the query instead of tablename

--- src/control/HomePageControl.class.hh
+++ src/control/HomePageControl.class.hh
@@ -9,7 +9,7 @@ use axolotl\util\Doctrine;
 class HomePageControl extends LoggedInPageControl{
   public function execute(): void{
     $entityManager = Doctrine::getEntityManager();
-    $user = $entityManager->find("users", _::SESSION("uid"));
+    $user = $entityManager->find("\\axolotl\\entities\\User", _::SESSION("uid"));
     (new HomePageView($user))->render();
   }
 }
