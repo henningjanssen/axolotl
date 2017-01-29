b2452a57f3097c0480b879acb7405be9357d75d4
Henning Janßen <development@henning-janssen.net>
2017-01-29 22:29:34 +0100

    Add Session::getCurrentUser
    Returns the currently logged in user or throws a NotLoggedInException

--- src/util/Session.class.hh
+++ src/util/Session.class.hh
@@ -2,6 +2,9 @@
 
 namespace axolotl\util;
 
+use \axolotl\entities\User;
+use \axolotl\exceptions\NotLoggedInException;
+
 class Session{
   public static function loggedIn(): bool{
     return _::SESSION("uid") !== null;
@@ -12,5 +15,16 @@ class Session{
       session_start();
     }
     session_destroy();
+  }
+
+  public static function getCurrentUser(): User{
+    if(!self::loggedIn()){
+      throw new NotLoggedInException();
+    }
+    $entityManager = Doctrine::getEntityManager();
+    $user = $entityManager->find(
+      "\\axolotl\\entities\\User", _::SESSION("uid")
+    );
+    return $user;
   }
 }
