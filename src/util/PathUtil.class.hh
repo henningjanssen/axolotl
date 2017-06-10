d2dbe5ed5d4af07b2430ebf3fd29f94f5a63dfd5
Henning Janßen <development@henning-janssen.net>
2017-06-11 01:14:13 +0200

    Add PathUtil for operations on paths and path-constants

---
+++ src/util/PathUtil.class.hh
@@ -0,0 +1,25 @@
+<?hh // strict
+
+namespace axolotl\util;
+
+class PathUtil{
+	const string MODULE_PATH = __DIR__.'/../../modules/';
+
+	public static function deleteDirectory(string $dir): void{
+		$delIt = new \RecursiveDirectoryIterator($dir,
+			\RecursiveDirectoryIterator::SKIP_DOTS
+		);
+		$delFiles = new \RecursiveIteratorIterator($delIt,
+			\RecursiveIteratorIterator::CHILD_FIRST
+		);
+		foreach($delFiles as $delf){
+			if($delf->isDir()){
+				rmdir($delf->getRealPath());
+			}
+			else{
+				unlink($delf->getRealPath());
+			}
+		}
+		rmdir($dir);
+	}
+}
