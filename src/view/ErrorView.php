82bae62c3cbac5bcabe76a780db41c8984942a1f
Henning Janßen <development@henning-janssen.net>
2018-02-20 23:40:37 +0100

    ErrorView: fix typo

--- src/view/ErrorView.php
+++ src/view/ErrorView.php
@@ -5,7 +5,7 @@ namespace axolotl\view;
 // Not extending PageView as no HTML-content has to be sent
 class ErrorView extends PageView{
   public function __construct(int $errno){
-    parent::construct("error.html", "Error");
+    parent::__construct("error.html", "Error");
     $this->errno = $errno;
     $this->setHTTPStatusCode($this->errno);
     $this->setVars(array('errno' => $errno));
