ea1e6891ed0d90f0e762c5f45ddc3eb66a4d01c8
Henning Janßen <development@henning-janssen.net>
2018-05-25 17:32:41 +0200

    Don't use form to delete users

--- src/control/ListUsersControl.php
+++ src/control/ListUsersControl.php
@@ -4,19 +4,22 @@ namespace axolotl\control;
 
 use axolotl\entities\User;
 use axolotl\view\ListUsersView;
+use axolotl\view\RedirectView;
 use axolotl\util\_;
 use axolotl\util\Doctrine;
 use axolotl\util\Session;
 
 class ListUsersControl extends LoggedInPageControl{
   public function execute(): void{
-    if(_::POST('delete') !== null){
-      $del = User::get(intval(_::POST('delete')));
+    if(_::GET('delete', -1) > 0){
+      $del = User::get(intval(_::GET('delete')));
       if($del){
         $em = Doctrine::getEntityManager();
         $em->remove($del);
         $em->flush();
       }
+      (new RedirectView('/users'))->render();
+      return;
     }
 
     $users = User::getAll();
