783eb895d1868403a9445a6ddfe56906b4794934
Henning Janßen <development@henning-janssen.net>
2018-05-25 14:05:51 +0200

    StreamView: optionalize everything

--- src/view/StreamView.php
+++ src/view/StreamView.php
@@ -7,20 +7,30 @@ use axolotl\util\_;
 // Not extending PageView as no HTML-content has to be sent
 class StreamView{
   public function __construct(
-    $stream, string $filename
+    $stream, string $filename, $forceDownload = true,
+    $contentType = "application/octet-stream", int $initialOffset = 0,
+    $flush = true
   ){
     $this->stream = $stream;
-    header("Content-Disposition: attachment; filename=\"$filename\"");
-    header("Content-Type: application/octet-stream");
+    $this->initialOffset = $initialOffset;
+    $this->flush = $flush;
+    $contentDisp = $forceDownload ?'attachment' :'inline';
+    header("Content-Disposition: $contentDisp; filename=\"$filename\"");
+    header("Content-Type: $contentType");
   }
 
   public function render(): void{
+    $offset = $this->initialOffset;
     while(true){
-      $cont = fread($this->stream, 1024);
+      $cont = stream_get_contents($this->stream, 1024, $offset);
+      $offset = -1;
       if($cont === false){
         break;
       }
-      print($cont);
+      echo $cont;
+      if($this->flush){
+        ob_flush();
+      }
     }
   }
 }
