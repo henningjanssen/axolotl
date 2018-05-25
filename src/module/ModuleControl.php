5f2ca9cecf76543432626837e15bd952ce196edc
Henning Janßen <development@henning-janssen.net>
2018-05-25 18:13:24 +0200

    module/ModuleControl: contain std-functions

--- src/module/ModuleControl.php
+++ src/module/ModuleControl.php
@@ -2,10 +2,38 @@
 
 namespace axolotl\module;
 
-interface ModuleControl{
-  public function install(): bool;
-  public function uninstall(): bool;
-  public function update(): bool;
-  public function backup(): void;
-  public function getRoutings(): array;
+use \Doctrine\ORM\Tools\SchemaTool;
+
+use axolotl\util\Doctrine;
+
+abstract class ModuleControl{
+  public function install(): bool{
+    $schemaTool = $this->getSchemaTool();
+    $classes = $this->getClasses();
+    $schemaTool->createSchema($classes);
+    return true;
+	}
+
+  public function uninstall(): bool{
+    $schemaTool = $this->getSchemaTool();
+    $classes = $this->getClasses();
+    $schemaTool->dropSchema($classes);
+    return true;
+	}
+
+  public function update(): bool{
+    $schemaTool = $this->getSchemaTool();
+    $classes = $this->getClasses();
+    $schemaTool->updateSchema($classes, true);
+    return true;
+	}
+
+  public function getSchemaTool(): SchemaTool{
+    $em = Doctrine::getEntityManager();
+    return new SchemaTool($em);
+  }
+
+  public function backup(): void{}
+  public abstract function getRoutings(): array;
+  public abstract function getClasses(): array;
 }
